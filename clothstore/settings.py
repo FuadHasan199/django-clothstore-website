306395037d1a2452a2b9a4d3eb78c3422533399a
MD Fuad <fuadhasa199@gmail.com>
2024-10-13 06:58:52 +0600

    requirment.txt file created

--- clothstore/settings.py
+++ clothstore/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d^*bks*6#i-)#-mh%s(8#ey5!%2_+&p9f9!&s8@mq*waq(%w5m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
